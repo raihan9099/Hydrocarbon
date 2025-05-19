d52ab8d75ec79f96870d68d348a7bc071a481d6c
TARIF-AHMED-404 <mohammadtarif787@gmail.com>
2025-05-19 11:43:49 +0600

    Create delete.js

---
+++ scripts/cmds/delete.js
@@ -0,0 +1,35 @@
+const { GoatWrapper } = require("fca-liane-utils");
+module.exports = {
+  config: {
+    name: "delete",
+    aliases: ["del"],
+    author: "TARIF AHMED",
+role: 2,
+    category: "OWNER"
+  },
+
+  onStart: async function ({ api, event, args }) {
+    const fs = require('fs');
+    const path = require('path');
+
+    const fileName = args[0];
+
+    if (!fileName) {
+      api.sendMessage("Please provide a file name to delete.", event.threadID);
+      return;
+    }
+
+    const filePath = path.join(__dirname, fileName);
+
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error(err);
+        api.sendMessage(`❎ | Failed to delete ${fileName}.`, event.threadID);
+        return;
+      }
+      api.sendMessage(`✅ ( ${fileName} ) Deleted successfully!`, event.threadID);
+    });
+  }
+};
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
