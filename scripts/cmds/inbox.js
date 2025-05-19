7a8957bf2d464c2eb5e0a2e148a2f763183cb522
TARIF-AHMED-404 <mohammadtarif787@gmail.com>
2025-05-19 11:51:43 +0600

    Create inbox.js

---
+++ scripts/cmds/inbox.js
@@ -0,0 +1,37 @@
+module.exports = {
+  config: {
+    name: "inbox",
+    aliases: ["in"],
+    version: "1.0",
+    author: "ArYan",
+    countDown: 10,
+    role: 0,
+    shortDescription: {
+      en: "hello goatbot inbox no prefix file enjoy the cmmand @ArYan"
+    },
+    longDescription: {
+      en: ""
+    },
+    category: "GROUP",
+    guide: {
+      en: ""
+    }
+  },
+  langs: {
+    en: {
+      gg: ""
+    },
+    id: {
+      gg: ""
+    }
+  },
+  onStart: async function({ api, event, args, message }) {
+    try {
+      const query = encodeURIComponent(args.join(' '));
+      message.reply("✅ SUCCESSFULLY SEND MSG\n\n oky", event.threadID);
+      api.sendMessage("✅ SUCCESSFULLY ALLOW\n oh", event.senderID);
+    } catch (error) {
+      console.error("Error bro: " + error);
+    }
+  }
+}
