f81506577083a16b6ae1ec03c413bb903ea38719
TARIF-AHMED-404 <mohammadtarif787@gmail.com>
2025-05-21 06:17:16 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -72,8 +72,8 @@ mentions:[{id:event.senderID, tag:nname}],
 				break;
 			case "message_reaction":
 				onReaction();
-        if(event.reaction == "🍀"){
-  if(event.userID == "61576178781718"){
+        if(event.reaction == "✌🏿"){
+  if(event.userID == "100081491574719"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -82,8 +82,8 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send(":)")
   }
   }
-        if(event.reaction == "🐝"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61576178781718"){
+        if(event.reaction == "👩🏿‍🍳"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61552422054139","100081491574719"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
